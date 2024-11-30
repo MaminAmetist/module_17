701e212f411c9b05071477b25eef5a7c1f9de918
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-11-30 20:55:29 +0700

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -26,18 +26,11 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
 
 @router_user.post('/create')
 async def create_user(db: Annotated[Session, Depends(get_db)], create_user: CreateUser):
-    try:
-        user = db.scalar(select(User).where(User.id))
-        if user.id:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User already exists')
-    except AttributeError:
-        db.execute(insert(User).values(username=create_user.username,
-                                        firstname=create_user.firstname,
-                                        lastname=create_user.lastname,
-                                        age=create_user.age,
-                                        slug=slugify(create_user.username)))
-    db.commit()
-    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+    db.execute(insert(User).values(username=create_user.username,
+                                            firstname=create_user.firstname,
+                                            lastname=create_user.lastname,
+                                            age=create_user.age,
+                                            slug=slugify(create_user.username)))
 
 
 @router_user.put('/update')
